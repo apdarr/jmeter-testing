4752ac65c8ba9e7ecfb995c13cd42a854ad34093
apdarr <alexdarr@gmail.com>
2017-04-07 11:55:56 -0400

    delete file path

--- test_plan.rb
+++ test_plan.rb
@@ -4,4 +4,4 @@ test do
     threads count: 10 do
       visit name: 'National Parks Home Page', url: 'https://national-parks-app.herokuapp.com'
     end
-end.run(path: '/vendor/apache-jmeter/bin/')
+end.run
