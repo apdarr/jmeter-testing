88f897e9810b1d49086ce4aecfb4ee80df0d54d0
apdarr <alexdarr@gmail.com>
2017-04-07 11:47:17 -0400

    fix typo

--- test_plan.rb
+++ test_plan.rb
@@ -1,8 +1,7 @@
 require 'ruby-jmeter'
-#jmeter test block
 
 test do
-    thread count: 10 do
+    threads count: 10 do
       visit name: 'National Parks Home Page', url: 'https://national-parks-app.herokuapp.com'
     end
 end.run(path: '/vendor/apache-jmeter/bin/')
