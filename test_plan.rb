457fbe8873d2aa89d99c04082507e736541d0df1
apdarr <alexdarr@gmail.com>
2017-04-07 14:38:58 -0400

    tweak file path

--- test_plan.rb
+++ test_plan.rb
@@ -7,4 +7,4 @@ test do
     threads count: 10 do
       visit name: 'National Parks Home Page', url: 'https://national-parks-app.herokuapp.com'
     end
-end.run(path:'/app/vendor/apache-jmeter/apache-jmeter-3.1/bin/ ')
+end.run(path:'/app/vendor/apache-jmeter/apache-jmeter-3.1/bin/')
